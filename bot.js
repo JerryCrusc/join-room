7c4c34136780dfdfc00369909aff00c01e710583
JerryCrusc <55596528+JerryCrusc@users.noreply.github.com>
2019-09-21 02:16:48 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,430 @@
+﻿const Discord = require("discord.js");
+const queue = new Map();
+const client = new Discord.Client();
+const bot = new Discord.Client();
+const mtra9a = new Discord.Client();
+const ytdl = require('ytdl-core');
+const Bot = new Discord.Client();
+const b = new Discord.Client();
+const c = new Discord.Client();
+const d = new Discord.Client();
+const e = new Discord.Client();
+const f = new Discord.Client();
+const g = new Discord.Client();
+const h = new Discord.Client();
+const i = new Discord.Client();
+const j = new Discord.Client();
+const k = new Discord.Client();
+const l = new Discord.Client();
+const m = new Discord.Client();
+const n = new Discord.Client();
+const o = new Discord.Client();
+const p = new Discord.Client();
+const q = new Discord.Client();
+const r = new Discord.Client();
+const s = new Discord.Client();
+const t = new Discord.Client();
+const u = new Discord.Client();
+const v = new Discord.Client();
+const w = new Discord.Client();
+const x = new Discord.Client();
+const y = new Discord.Client();
+const z = new Discord.Client();
+const a = "458997221170479124"
+
+
+client.on("ready", () => {
+console.log('By Cutie Pie');
+ console.log(`Logged in as ${client.user.tag}!`);
+});
+bot.on('message', msg => {
+ if (msg.author.id !== a) return;
+ if (msg.content.startsWith('.')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.join()
+}
+});
+bot.on('message', msg => {
+ if (msg.author.id !== a) return;
+ if (msg.content.startsWith('..')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.leave()
+}
+});
+mtra9a.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+mtra9a.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+Bot.on('message', msg => {
+ if (msg.author.id !== a) return;
+ if (msg.content.startsWith('.')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.join()
+}
+});
+Bot.on('message', msg => {
+ if (msg.author.id !== a) return;
+ if (msg.content.startsWith('..')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.leave()
+}
+});
+b.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+b.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+c.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+c.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+d.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+d.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+e.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+e.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+f.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+f.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+g.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+g.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+h.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+h.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+i.on('message', msg => {
+ if (msg.author.id !== a) return;
+ if (msg.content.startsWith('.')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.join()
+}
+});
+i.on('message', msg => {
+ if (msg.author.id !== a) return;
+ if (msg.content.startsWith('..')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.leave()
+}
+});
+j.on('message', msg => {
+ if (msg.author.id !== a) return;
+ if (msg.content.startsWith('.')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.join()
+}
+});
+j.on('message', msg => {
+ if (msg.author.id !== a) return;
+ if (msg.content.startsWith('..')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.leave()
+}
+});
+k.on('message', msg => {
+ if (msg.author.id !== a) return;
+ if (msg.content.startsWith('.')) {
+		if (!msg.member.voiceChannel) return
+		msg.member.voiceChannel.join()
+}
+});
+k.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+l.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+l.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+m.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+m.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+o.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+o.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+p.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+p.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+q.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+q.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+r.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+r.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+s.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+s.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+t.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+t.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+u.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+u.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+v.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+v.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+w.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+w.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+x.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+x.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+y.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+y.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+z.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('.')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.join()
+   }
+   });
+z.on('message', msg => {
+	if (msg.author.id !== a) return;
+	if (msg.content.startsWith('..')) {
+		   if (!msg.member.voiceChannel) return
+		   msg.member.voiceChannel.leave()
+   }
+   });
+
+client.on('message', message => {
+  if (message.content.startsWith('hah')) {
+    const voiceChannel = message.member.voiceChannel;
+    voiceChannel.join()
+      .then(connnection => {
+        const stream = ytdl("https://www.youtube.com/watch?v=HHn2tvazfTk", { filter: 'audioonly' });
+        const dispatcher = connnection.playStream(stream);
+                dispatcher.on('end', () => voiceChannel.leave());
+
+      });
+  }
+})
